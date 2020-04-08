baf46b80eab5c8a308064d18f896ce24757199f6
Luca Stefani <theblackloki@gmail.com>
2020-04-08 22:28:37 +0200

    Milestone 2

--- javascript/main.js
+++ javascript/main.js
@@ -3,21 +3,50 @@
 
 $(document).ready(function(){
 
-$(".fa-paper-plane").hide()
+$(".fa-paper-plane").hide();
 $("#invio").keypress(function () {
-  $(".fa-paper-plane").show()
-  $(".fa-microphone").hide()
+  $(".fa-paper-plane").show();
+  $(".fa-microphone").hide();
 })
 
-// Utente scrive messaggio nel box e preme sul send-->il messaggio viene stampato a destra in verde
+// Utente scrive messaggio nel box e preme sul send o invio-->il messaggio viene stampato a destra in verde e dopo un secondo viene stampata la risposta
 
-$(".fa-paper-plane").click(function () {
-  var messaggio = $("#invio").val()
-
-$(".chatMsg").append("<div class='msg sent'><p>" + messaggio + "</p></div>");
-
-  $("#invio").val("");
-  $(".fa-paper-plane").hide()
-  $(".fa-microphone").show()
+$(".fa-paper-plane").click(invioMsg)
+$('#invio').keypress(function(event) {
+  var key = event.which;
+  if (key === 13) {
+    invioMsg()
+  }
 })
+// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite
+$(".inpRicerca").keyup(function () {
+  $(".chatSinistra").show()
+  var testoMaiusc = $('.inpRicerca').val()
+  var testo = testoMaiusc.charAt(0).toUpperCase() + testoMaiusc.slice(1);
+  console.log(testo);
+  $(".nomeContatto").each(function () {
+    var questo = $(this).text()
+    var padre = $(this).parent()
+    var nonno = padre.parent()
+    if (questo.includes(testo)) {
+      console.log(questo);
+      nonno.parent().show()
+    }else {
+      nonno.parent().hide()
+    }
+  })
+
+})
+//----------------FUNZIONI-----------//
+  function invioMsg() {
+    var messaggio = $("#invio").val()
+    $(".chatMsg").append("<div class='msg sent'><p>" + messaggio + "</p></div>");
+    $("#invio").val("");
+    $(".fa-paper-plane").hide();
+    $(".fa-microphone").show();
+    setTimeout(risposta, 1000);
+  }
+  function risposta() {
+    $(".chatMsg").append("<div class='msg recived'><p>ok</p></div>")
+  }
 })
